8a3c6a7ca3c7f1cae5a3fcda7b34ae8305065402
Dgzz <al45764@un.edu.mx>
2025-12-02 10:43:20 -0600

    script

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var nombre = document.getElementById("Nombre");
-var apellido = document.getElementById("Ocupacion");
-var correo = document.getElementById(".perfilcard");
+var apellido = document.getElementById("ocupacion");
 
 var esMasculino = true
 
 perfilCard.onclick = function() {
-    if (esMasculino) {
-    
+    if (esMasculino) 
+    nombre.textcontent = "Omarcita";
+    ocupacion.textcontent = "Estudiante Universitaria y jugadora de basketball";
+} else {
+    nombre.textcontent = "Omar";
+    ocupacion.textcontent = "Estudiante Universitario y jugador de basketball";
+}
