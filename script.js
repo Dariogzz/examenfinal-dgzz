b77be167cc768fb2d49d0d3fd763c1ed4ceb81bb
Dgzz <al45764@un.edu.mx>
2025-12-02 10:25:00 -0600

    intento de script pero no puedo

---
+++ script.js
@@ -0,0 +1,9 @@
+var nombre = document.getElementById("Nombre");
+var apellido = document.getElementById("Ocupacion");
+var correo = document.getElementById(".perfilcard");
+
+var esMasculino = true
+
+perfilCard.onclick = function() {
+    if (esMasculino) {
+    
